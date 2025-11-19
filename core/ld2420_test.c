abace7fdd7705d36944fce709f70593169c6993f
Mikayel Grigoryan <michael.grigoryan25@gmail.com>
2025-11-19 11:23:26 +0400

    refactor: remove unused byteswap function from ld2420 test file

--- core/ld2420_test.c
+++ core/ld2420_test.c
@@ -1,14 +1,6 @@
 #include <unity.h>
 #include <ld2420/ld2420.h>
 
-uint32_t byteswap_uint32(uint32_t x)
-{
-    return ((x >> 24) & 0xff) |
-           ((x >> 8) & 0xff00) |
-           ((x << 8) & 0xff0000) |
-           ((x << 24) & 0xff000000);
-}
-
 void setUp(void)
 {
 }
